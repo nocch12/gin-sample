45f6b4ce0126a8568749d06687e6f06cd873203a
Kohki Ohhno <k.ohno12.gt@gmail.com>
2022-09-16 21:18:55 +0900

    first commit

---
+++ controller/video-controller.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nocch12/gin-sample/entity"
+	"github.com/nocch12/gin-sample/service"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) entity.Video
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+func New(service service.VideoService) VideoController {
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	ctx.BindJSON(&video)
+	c.service.Save(video)
+	return video
+}
